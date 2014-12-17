7c0c2ecee327f85d386e6a35cefdaf0a416c892e
sumitc91 <sumitc91@26c8ff76-45bc-40fb-aa89-bc4668cafcb9>
2014-12-17 08:58:58 +0000

    M2EMobile Commit

---
+++ M2EMobile/M2EMobile/App.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using M2EMobile.Services;
+using Xamarin.Forms;
+
+namespace M2EMobile
+{
+    public enum UIImplementation
+    {
+        CSharp = 0,
+        Xaml
+    }
+
+    public class App
+    {
+        //Change the following line to switch between XAML and C# versions
+        private static UIImplementation uiImplementation = UIImplementation.CSharp;
+
+        public static IDirectoryService Service { get; set; }
+
+        public static IPhoneFeatureService PhoneFeatureService { get; set; }
+
+        public static DateTime LastUseTime { get; set; }
+
+        public static Page GetMainPage()
+        {
+            var employeeList = new ContentPage();
+            if (uiImplementation == UIImplementation.CSharp)
+            {
+                employeeList = new UserHomeView();
+            }
+            else if (uiImplementation == UIImplementation.Xaml)
+            {
+                //employeeList = new EmployeeListXaml();
+            }
+
+            return new NavigationPage(employeeList);
+        }
+    }
+}
