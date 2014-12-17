e0fc0685f85ac3e9a033fde637bdd2d1846987b3
sumitc91 <sumitc91@26c8ff76-45bc-40fb-aa89-bc4668cafcb9>
2014-12-17 09:31:21 +0000

    xamarin mobile app

--- branches/M2EMobile/M2EMobile/M2EMobile/Views/UserHomeView.cs
+++ branches/M2EMobile/M2EMobile/M2EMobile/Views/UserHomeView.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using M2EMobile.ViewModels;
 using Xamarin.Forms;
 
 namespace M2EMobile.Views
@@ -10,5 +11,21 @@ namespace M2EMobile.Views
     class UserHomeView : ContentPage
     {
 
+        public UserHomeView()
+        {
+            Content = new Label
+            {
+                Text = "Hello,New Forms !",
+                VerticalOptions = LayoutOptions.CenterAndExpand,
+                HorizontalOptions = LayoutOptions.CenterAndExpand,
+            };
+        }
+
+        protected async override void OnAppearing()
+        {
+            base.OnAppearing();
+            if (LoginViewModel.ShouldShowLogin(App.LastUseTime))
+                await Navigation.PushModalAsync(new LoginView());
+        }
     }
 }
