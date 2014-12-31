1af8ae5da62890d145a172614b6664fcb485cd58
sumitc91 <sumitc91@26c8ff76-45bc-40fb-aa89-bc4668cafcb9>
2014-12-31 07:15:54 +0000

    async http post implementation

---
+++ M2EMobile/M2EMobile/Models/DataResponse/UserResponse/UserMessageList.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace M2EMobile.Models.DataResponse.UserResponse
+{
+    public class UserMessageList
+    {
+        public string link { get; set; }
+        public string imageUrl { get; set; }
+        public string messageTitle { get; set; }
+        public string MessagePostedInTimeAgo { get; set; }
+        public string messageContent { get; set; }
+        public string MessageSeen { get; set; }
+    }
+}
---
+++ M2EMobile/M2EMobile/Models/DataResponse/UserResponse/UserMessagesResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M2EMobile.Models.DataResponse.UserResponse
+{
+    public class UserMessagesResponse
+    {
+        public string UnreadMessages { get; set; }
+        public string CountLabelType { get; set; }
+        public List<UserMessageList> MessageList { get; set; }
+    }
+}
