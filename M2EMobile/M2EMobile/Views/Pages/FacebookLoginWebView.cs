83484295f3aad00546459e35d9ff666d29b0e180
sumitc91 <sumitc91@26c8ff76-45bc-40fb-aa89-bc4668cafcb9>
2015-01-07 13:08:55 +0000

    Mobile App Facebook login implemented.

--- M2EMobile/M2EMobile/Views/Pages/FacebookLoginWebView.cs
+++ M2EMobile/M2EMobile/Views/Pages/FacebookLoginWebView.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Facebook;
+using M2EMobile.Models.DataWrapper;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using RestSharp.Portable.Deserializers;
 using Xamarin.Forms;
 
 namespace M2EMobile.Views.Pages
@@ -15,21 +19,10 @@ namespace M2EMobile.Views.Pages
             var fb = new FacebookClient(accessToken);
                     dynamic result = fb.Get("fql",
                                 new { q = "SELECT uid, first_name, last_name, sex, pic_big_with_logo, username FROM user WHERE uid=me()" });
-
-            var user = new M2EMobile.Models.User();
-            {
-                user.Username = result.data[0].username + "@facebook.com";
-                        //Password = EncryptionClass.Md5Hash(Guid.NewGuid().ToString()),
-                        user.Source = "facebook";
-                        user.isActive = "true";
-                        user.Type = "user";
-                        user.guid = Guid.NewGuid().ToString();
-                        user.fixedGuid = Guid.NewGuid().ToString();
-                        user.FirstName = result.data[0].first_name;
-                        user.LastName = result.data[0].last_name;
-                        user.gender = result.data[0].sex;
-                        user.ImageUrl = result.data[0].pic_big_with_log;
-                    };
+          
+            dynamic dynamicJsonResult = JObject.Parse(result);
+            var userDetail = JsonConvert.DeserializeObject<List<FacebookUserDetailAPIResponseWrapper>>(dynamicJsonResult.data);
+            
         }
     }
 }
